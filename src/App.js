8ca8ad212a43a0433e948791b0516fb405423298
hyeonukdev <lego7407@gmail.com>
2022-03-21 16:49:18 +0900

    :sparkles: Add Route and pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import RouteTest from "./components/RouteTest";
+
+import Home from "./pages/Home";
+import Edit from "./pages/Edit";
+import New from "./pages/New";
+import Diary from "./pages/Diary";
 
 function App() {
-  return <div className="App">TEST</div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new" element={<New />} />
+          <Route path="/edit" element={<Edit />} />
+          <Route path="/diary" element={<Diary />} />
+        </Routes>
+        <RouteTest />
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
