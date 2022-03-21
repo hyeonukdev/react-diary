d2479883e4996c7e648618c821813175583e0b58
hyeonukdev <lego7407@gmail.com>
2022-03-21 18:33:47 +0900

    :lipstick: Add common-Header

--- src/App.js
+++ src/App.js
@@ -8,11 +8,21 @@ import Diary from "./pages/Diary";
 
 //components
 import MyButton from "./components/MyButton";
+import MyHeader from "./components/MyHeader";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <MyHeader
+          headText={"App"}
+          leftChild={
+            <MyButton text={"왼쪽버튼"} onClick={() => alert("왼쪽클릭")} />
+          }
+          rightChild={
+            <MyButton text={"오른쪽버튼"} onClick={() => alert("오른쪽클릭")} />
+          }
+        />
         <h2>DIARY APP</h2>
         <MyButton
           text={"버튼"}
