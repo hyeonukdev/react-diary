b08876ab2dd58ac2475bd5e708eb56681f5e380a
hyeonukdev <lego7407@gmail.com>
2022-03-22 15:42:41 +0900

    :fire: Remove code

--- src/App.js
+++ src/App.js
@@ -6,44 +6,10 @@ import Edit from "./pages/Edit";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
 
-//components
-import MyButton from "./components/MyButton";
-import MyHeader from "./components/MyHeader";
-
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <MyHeader
-          headText={"App"}
-          leftChild={
-            <MyButton text={"왼쪽버튼"} onClick={() => alert("왼쪽클릭")} />
-          }
-          rightChild={
-            <MyButton text={"오른쪽버튼"} onClick={() => alert("오른쪽클릭")} />
-          }
-        />
-        <h2>DIARY APP</h2>
-        <MyButton
-          text={"버튼"}
-          onClick={() => alert("버튼 클릭")}
-          type={"123"}
-        />
-        <MyButton
-          text={"버튼"}
-          onClick={() => alert("버튼 클릭")}
-          type={"positive"}
-        />
-        <MyButton
-          text={"버튼"}
-          onClick={() => alert("버튼 클릭")}
-          type={"negative"}
-        />
-        <MyButton
-          text={"버튼"}
-          onClick={() => alert("버튼 클릭")}
-          type={"default"}
-        />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
