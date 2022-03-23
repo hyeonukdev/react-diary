dc936a556c4127c90f5d0f71e8131c0b350c039f
hyeonukdev <lego7407@gmail.com>
2022-03-23 17:36:23 +0900

    :construction: Add New page

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,8 +1,36 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import MyHeader from "../components/MyHeader";
+import MyButton from "../components/MyButton";
+
+const getStringDate = (date) => {
+  return date.toISOString().slice(0, 10);
+};
+
 const New = () => {
+  const [date, setDate] = useState(getStringDate(new Date()));
+  const navigate = useNavigate();
   return (
     <div>
-      <h1>New</h1>
-      <p>This page is New</p>
+      <MyHeader
+        headText={"새 일기쓰기"}
+        leftChild={
+          <MyButton text={"< 뒤로가기"} onClick={() => navigate(-1)} />
+        }
+      />
+      <div>
+        <section>
+          <h4>오늘은 언제인가요?</h4>
+          <div className="input-box">
+            <input
+              type="date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+            ></input>
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
