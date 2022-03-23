6b06676fbb928b97275e5764d593e7532307f8b2
hyeonukdev <lego7407@gmail.com>
2022-03-23 13:45:12 +0900

    :sparkles: Add setSortType

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -9,7 +9,7 @@ const ControlMenu = ({ value, onChange, optionList }) => {
   return (
     <select value={value} onChange={(e) => onChange(e.target.value)}>
       {optionList.map((it, idx) => (
-        <option key={idx} valu={it.value}>
+        <option key={idx} value={it.value}>
           {it.name}
         </option>
       ))}
@@ -19,6 +19,24 @@ const ControlMenu = ({ value, onChange, optionList }) => {
 
 const DiaryList = ({ diaryList }) => {
   const [sortType, setSortType] = useState("latest");
+
+  // 원본에 영향을 주지 않도록 string -> array 처리 후 정렬
+  const getProcessDiaryList = () => {
+    const compare = (a, b) => {
+      console.log("sortType: ", sortType);
+      if (sortType === "latest") {
+        return parseInt(b.date) - parseInt(a.date);
+      } else {
+        return parseInt(a.date) - parseInt(b.date);
+      }
+    };
+    const copyList = JSON.parse(JSON.stringify(diaryList));
+    console.log("copyList: ", copyList);
+    const sortedDiaryList = copyList.sort(compare);
+    console.log("sortedDiaryList: ", sortedDiaryList);
+    return sortedDiaryList;
+  };
+
   return (
     <div>
       <ControlMenu
@@ -26,7 +44,7 @@ const DiaryList = ({ diaryList }) => {
         onChange={setSortType}
         optionList={sortOptionList}
       />
-      {diaryList.map((it) => (
+      {getProcessDiaryList().map((it) => (
         <div key={it.id}>{it.content}</div>
       ))}
     </div>
